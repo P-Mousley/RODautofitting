a35101fc92fdf3d532bdbd2237606c6b532b7ac4
Philip Mousley <philipmousley@hotmail.com>
2024-10-11 09:58:55 +0100

    add in missing variables for asaloop and runloop

--- RODautofitting.py
+++ RODautofitting.py
@@ -1,5 +1,7 @@
 import os,time,shutil
 import subprocess
+import numpy as np
+import pandas as pd
 def addinitmacline(macline,templateinitfile,initfile):
 
     f=open(templateinitfile)
@@ -77,47 +79,49 @@ def calcfitlist(run,n):
 
 
 
-def createmacroASA(files,startvals,donefile):
+def createmacroASA(mname,files,startvals,donefile):
     f=open(fr"{files['fitmac']}",'w')
     f.write(fr"re bul {files['bul']}"+'\n')
     f.write(fr"re dat  {files['dat']}"+'\n')
     f.write('mac sfsetup'+'\n')
     f.write(fr"mac {files['loadmac']} return return"+'\n')
+    outname=mname.replace('.sur','')
     f.write(fr"re fit {files['fit']}"+'\n')
     f.write(fr"re par {startvals}"+'\n')
     f.write('mac fitlogloop5'+'\n')
     f.write(fr"li par {donefile} done")
     f.close()
     
-def createmacroRUN(files,startvals,donefile):
+def createmacroRUN(mname,files,startvals,donefile):
     f=open(fr"{files['fitmac']}",'w')
     f.write(fr"re bul {files['bul']}"+'\n')
     f.write(fr"re dat  {files['dat']}"+'\n')
     f.write('mac sfsetup'+'\n')
     f.write(fr"mac {files['loadmac']} return return"+'\n')
+    outname=mname.replace('.sur','')
     f.write(fr"re fit {files['fit']}"+'\n')
     f.write(fr"re par {startvals}"+'\n')
     f.write('mac fitlogloop5run'+'\n')
     f.write(fr"li par {donefile} done")
     f.close()
 
-def ASAloop(fitlist,files,fit5path,donefile,rodexe):
+def ASAloop(modname,fitlist,files,fit5path,donefile,rodexe,asacount,run):
     while checkchidiff(fitlist)>0.01:
         startvals=fit5path
-        createmacroASA(files,startvals,donefile)
+        createmacroASA(modname,files,startvals,donefile)
         domacrofits(donefile,rodexe)
         asacount+=1
         print(f'asa loop {asacount}')
         fitlist,fitted,ranges,fitdf=calcfitlist(run,5)
-    return fitlist[4]
+    return fitlist[4],asacount
 
-def runloop(fitlist,files,fit5path,donefile,rodexe):
+def runloop(modname,fitlist,files,fit5path,donefile,rodexe,runcount,run):
     while checkchidiff(fitlist)>0.005:
         startvals=fit5path
-        createmacroRUN(files,startvals,donefile)
+        createmacroRUN(modname,files,startvals,donefile)
         domacrofits(donefile,rodexe)
         runcount+=1
         print(f'run loop {runcount}')
         fitlist,fitted,ranges,fitdf=calcfitlist(run,5)
-    return fitlist[4]
+    return fitlist[4],runcount
     
